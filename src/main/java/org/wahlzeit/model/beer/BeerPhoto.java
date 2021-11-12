ab27bd5fe8bac683e9a690fe9893b41c05cd54c6
paukre_fedora <paulkremer@fau.de>
2021-11-12 17:04:00 +0100

    BeerPhoto functionality implemented

--- src/main/java/org/wahlzeit/model/beer/BeerPhoto.java
+++ src/main/java/org/wahlzeit/model/beer/BeerPhoto.java
@@ -1,6 +1,7 @@
 package org.wahlzeit.model.beer;
 
 import org.wahlzeit.model.Photo;
+import org.wahlzeit.model.PhotoId;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,10 +14,40 @@ public class BeerPhoto extends Photo {
     public static final String ALCOHOLIC_STRENGTH = "alcoholicStrength";
     public static final String BEER_STYLE = "beer_style"; //e.g. https://hobbybrauer.de/forum/wiki/doku.php/biertypen
 
+    protected String cityOfOrigin;
+    protected double originalWort;
+    protected int yearEstablished;
+    protected double alcoholicStrength;
+    protected BeerStyle beerStyle;
+
+
 
     @Override
     public void readFrom(ResultSet rset) throws SQLException {
+        cityOfOrigin = rset.getString(CITY_OF_ORIGIN);
+        originalWort = rset.getDouble(ORIGINAL_WORT);
+        yearEstablished = rset.getInt(YEAR_ESTABLISHED);
+        alcoholicStrength = rset.getDouble(ALCOHOLIC_STRENGTH);
+        beerStyle = BeerStyle.getByKey(rset.getString(BEER_STYLE));
         super.readFrom(rset);
+    }
 
+
+    @Override
+    public void writeOn(ResultSet rset) throws SQLException {
+        rset.updateString(CITY_OF_ORIGIN, cityOfOrigin);
+        rset.updateDouble(ORIGINAL_WORT, originalWort);
+        rset.updateInt(YEAR_ESTABLISHED, yearEstablished);
+        rset.updateDouble(ALCOHOLIC_STRENGTH, alcoholicStrength);
+        rset.updateString(BEER_STYLE, beerStyle.getKey());
+        super.writeOn(rset);
+    }
+
+    public BeerPhoto(PhotoId myId){
+        super(myId);
+    }
+
+    public BeerPhoto(ResultSet rset) throws SQLException {
+        readFrom(rset);
     }
 }
---
+++ src/main/java/org/wahlzeit/model/beer/BeerStyle.java
@@ -0,0 +1,38 @@
+package org.wahlzeit.model.beer;
+
+import java.security.InvalidParameterException;
+
+//This enum manages the different BeerStyles
+public enum BeerStyle {
+
+    //The different values are initialized
+    //A string parameter is given in the constructor to
+    LAGER("lager"),
+    PILSENER("pilsener"),
+    BOCK("bock"),
+    WEIZEN("weizen");
+
+    //the key variable is used to identify the Enum values
+    private final String key;
+
+    //the constructor sets the given argument to the enum variable
+    BeerStyle(String key){
+        this.key = key;
+    }
+
+    //Helper Method to get the key for a specific BeerStyle
+    public String getKey() {
+        return key;
+    }
+
+    //static helper method to get the BeerStyle by key
+    public static BeerStyle getByKey(String key){
+        for (BeerStyle beerStyle : BeerStyle.values()) {
+            if(beerStyle.getKey().equals(key)){
+                return beerStyle;
+            }
+        }
+        //if no BeerStyle for the specific key is found, an IllegalArgumentException is thrown
+        throw new IllegalArgumentException("not a valid key");
+    }
+}
